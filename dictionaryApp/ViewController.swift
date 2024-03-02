4d67a4fb7d62190aa87cc7ef8e69aa0daea08169
pudramusic <flyandseeru@gmail.com>
2024-03-02 18:33:45 +0300

    Initial Commit

---
+++ dictionaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dictionaryApp
+//
+//  Created by Yo on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
